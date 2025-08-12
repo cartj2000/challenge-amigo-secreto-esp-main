c2dae7b5fab21bd2d0eccb07333f748919603a5b
cartj2000 <cartj2000@hotmail.com>
2025-08-12 08:59:09 -0500

    función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function limpiarCampoDeEntrada() {
+document.querySelector("#amigo").value = "";
+}
+
+function agregarAmigo(nombre) {
+    let nombre = document.getElementById("amigo").value;
+    if (nombre.trim() == "") {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nombre);
+        limpiarCampoDeEntrada();
+    }
+}
+
+function asignarTextoAElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.textContent = texto;
+return;
+}
